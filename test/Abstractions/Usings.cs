0c73f0f341402bd51ea5876f09f65ee424abc3d6
Simon Novak <snovak7@gmail.com>
2023-06-24 19:06:19 +0200

    chore: Added Usings.cs

---
+++ test/Abstractions/Usings.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+#pragma warning disable SA1200
+global using Xunit;
+#pragma warning restore SA1200
