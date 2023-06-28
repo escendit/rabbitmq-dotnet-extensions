d92bbc3d9ce9e4d2de2346ba6c065c6ad17705d8
Simon Novak <snovak7@gmail.com>
2023-06-28 18:49:44 +0200

    chore: Updated HostBuilderTests.cs

--- test/StreamProtocol/HostBuilderTests.cs
+++ test/StreamProtocol/HostBuilderTests.cs
@@ -1,5 +1,6 @@
 using Escendit.Orleans.Clients.RabbitMQ.Abstractions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Orleans.Runtime;
@@ -8,6 +9,9 @@ using Xunit.Categories;
 
 namespace Escendit.Orleans.Clients.RabbitMQ.StreamProtocol.Tests;
 
+/// <summary>
+/// Host Builder Tests.
+/// </summary>
 public class HostBuilderTests
 {
     private readonly IHostBuilder _hostBuilder;
@@ -22,7 +26,7 @@ public class HostBuilderTests
     
     [Fact]
     [IntegrationTest]
-    public void Test1()
+    public void Test_GetRequiredService_StreamSystem_Default_FromConfigSectionPath()
     {
         var host = _hostBuilder
             .ConfigureAppConfiguration(builder =>
@@ -35,6 +39,113 @@ public class HostBuilderTests
         Assert.NotNull(host);
 
         var streamSystem = host.Services.GetRequiredServiceByName<StreamSystem>(ConnectionOptions.DefaultKey);
+        
+        Assert.NotNull(streamSystem);
+    }
+
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_StreamSystem_Default_FromOptions()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqStreamSystemAsDefault(options =>
+            {
+                options.Endpoints.Add(new Endpoint { HostName = "localhost" });
+                options.UserName = "guest";
+                options.Password = "guest";
+                options.VirtualHost = "/";
+            })
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var streamSystem = host.Services.GetRequiredServiceByName<StreamSystem>(ConnectionOptions.DefaultKey);
+        
+        Assert.NotNull(streamSystem);
+    }
+    
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_StreamSystem_Default_FromOptionsBuilder()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqStreamSystemAsDefault(options => options.BindConfiguration("Path"))
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var streamSystem = host.Services.GetRequiredServiceByName<StreamSystem>(ConnectionOptions.DefaultKey);
+        
+        Assert.NotNull(streamSystem);
+    }
+
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_StreamSystem_Test_FromConfigSectionPath()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqStreamSystem("test", "Path")
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var streamSystem = host.Services.GetRequiredServiceByName<StreamSystem>("test");
+        
+        Assert.NotNull(streamSystem);
+    }
+
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_StreamSystem_Test_FromOptions()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqStreamSystem("test", options =>
+            {
+                options.Endpoints.Add(new Endpoint { HostName = "localhost" });
+                options.UserName = "guest";
+                options.Password = "guest";
+                options.VirtualHost = "/";
+            })
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var streamSystem = host.Services.GetRequiredServiceByName<StreamSystem>("test");
+        
+        Assert.NotNull(streamSystem);
+    }
+    
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_StreamSystem_Test_FromOptionsBuilder()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqStreamSystem("test", options => options.BindConfiguration("Path"))
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var streamSystem = host.Services.GetRequiredServiceByName<StreamSystem>("test");
         
         Assert.NotNull(streamSystem);
     }
