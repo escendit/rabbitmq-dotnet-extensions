f7b971516a4d599505a9c2bb4eb5cc53bb68e840
Simon Novak <snovak7@gmail.com>
2023-06-28 18:49:28 +0200

    chore: Updated HostBuilderTests.cs

---
+++ test/AmqpProtocol/HostBuilderTests.cs
@@ -0,0 +1,106 @@
+using Escendit.Orleans.Clients.RabbitMQ.Abstractions;
+using Escendit.Orleans.Clients.RabbitMQ.AmqpProtocol;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Orleans.Runtime;
+using RabbitMQ.Client;
+using Xunit.Categories;
+
+namespace AmqpProtocol;
+
+public class HostBuilderTests
+{
+    private readonly IHostBuilder _hostBuilder;
+    
+    public HostBuilderTests()
+    {
+        _hostBuilder = Host
+            .CreateDefaultBuilder()
+            .ConfigureServices(services => services
+                .TryAddSingleton(typeof(IKeyedServiceCollection<,>), typeof(KeyedServiceCollection<,>)));
+    }
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_ConnectionFactory_Default_FromConfigSectionPath()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqConnectionFactory(ConnectionOptions.DefaultKey, "Path")
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>(ConnectionOptions.DefaultKey);
+        
+        Assert.NotNull(connectionFactory);
+    }
+
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_ConnectionFactory_Default_FromOptions()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqConnectionFactory(ConnectionOptions.DefaultKey, options =>
+            {
+                options.Endpoints.Add(new Endpoint { HostName = "localhost" });
+                options.UserName = "guest";
+                options.Password = "guest";
+                options.VirtualHost = "/";
+            })
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>(ConnectionOptions.DefaultKey);
+        
+        Assert.NotNull(connectionFactory);
+    }
+
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_ConnectionFactory_Default_FromOptionsBuilder()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqConnectionFactory(ConnectionOptions.DefaultKey, options => options.BindConfiguration("Path"))
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>(ConnectionOptions.DefaultKey);
+        
+        Assert.NotNull(connectionFactory);
+    }
+
+    [Fact]
+    [IntegrationTest]
+    public void Test_GetRequiredService_ConnectionFactory_Default_FromOption_Test()
+    {
+        var host = _hostBuilder
+            .ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"{Environment.CurrentDirectory}/../../../appsettings.json");
+            })
+            .AddRabbitMqConnectionOptionsAsDefault("Path")
+            .AddRabbitMqConnectionFactoryFromOption("test", ConnectionOptions.DefaultKey)
+            .Build();
+        
+        Assert.NotNull(host);
+
+        var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>("test");
+        
+        Assert.NotNull(connectionFactory);
+    }
+}
