f2aa27de505f6bdd9d94f4f86af3197ff824fdd0
Simon Novak <snovak7@gmail.com>
2023-06-29 17:45:40 +0200

    chore: Updated HostBuilderTests.cs

--- test/AmqpProtocol/HostBuilderTests.cs
+++ test/AmqpProtocol/HostBuilderTests.cs
@@ -1,19 +1,28 @@
-using Escendit.Orleans.Clients.RabbitMQ.Abstractions;
-using Escendit.Orleans.Clients.RabbitMQ.AmqpProtocol;
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clients.RabbitMQ.AmqpProtocol.Tests;
+
+using Abstractions;
+using AmqpProtocol;
+using global::Orleans.Runtime;
+using global::RabbitMQ.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using Orleans.Runtime;
-using RabbitMQ.Client;
 using Xunit.Categories;
 
-namespace AmqpProtocol;
-
+/// <summary>
+/// Host Builder Tests.
+/// </summary>
 public class HostBuilderTests
 {
     private readonly IHostBuilder _hostBuilder;
-    
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HostBuilderTests"/> class.
+    /// </summary>
     public HostBuilderTests()
     {
         _hostBuilder = Host
@@ -21,7 +30,10 @@ public class HostBuilderTests
             .ConfigureServices(services => services
                 .TryAddSingleton(typeof(IKeyedServiceCollection<,>), typeof(KeyedServiceCollection<,>)));
     }
-    
+
+    /// <summary>
+    /// Test GetRequiredService for ConnectionFactory Default FromConfigSectionPath.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_ConnectionFactory_Default_FromConfigSectionPath()
@@ -33,14 +45,17 @@ public class HostBuilderTests
             })
             .AddRabbitMqConnectionFactory(ConnectionOptions.DefaultKey, "Path")
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>(ConnectionOptions.DefaultKey);
-        
+
         Assert.NotNull(connectionFactory);
     }
 
+    /// <summary>
+    /// Test GetRequiredService ConnectionFactory Default FromOptions.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_ConnectionFactory_Default_FromOptions()
@@ -58,14 +73,17 @@ public class HostBuilderTests
                 options.VirtualHost = "/";
             })
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>(ConnectionOptions.DefaultKey);
-        
+
         Assert.NotNull(connectionFactory);
     }
 
+    /// <summary>
+    /// Test GetRequiredService ConnectionFactory Default FromOptionsBuilder.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_ConnectionFactory_Default_FromOptionsBuilder()
@@ -77,14 +95,17 @@ public class HostBuilderTests
             })
             .AddRabbitMqConnectionFactory(ConnectionOptions.DefaultKey, options => options.BindConfiguration("Path"))
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>(ConnectionOptions.DefaultKey);
-        
+
         Assert.NotNull(connectionFactory);
     }
 
+    /// <summary>
+    /// Test GetRequiredService ConnectionFactory Default FromOption Test.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_ConnectionFactory_Default_FromOption_Test()
@@ -97,14 +118,17 @@ public class HostBuilderTests
             .AddRabbitMqConnectionOptionsAsDefault("Path")
             .AddRabbitMqConnectionFactoryFromOption("test", ConnectionOptions.DefaultKey)
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connectionFactory = host.Services.GetRequiredServiceByName<IConnectionFactory>("test");
-        
+
         Assert.NotNull(connectionFactory);
     }
-    
+
+    /// <summary>
+    /// Test GetRequiredService Connection Default FromConfigSectionPath.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_Connection_Default_FromConfigSectionPath()
@@ -116,13 +140,17 @@ public class HostBuilderTests
             })
             .AddRabbitMqConnection(ConnectionOptions.DefaultKey, "Path")
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connection = host.Services.GetRequiredServiceByName<IConnection>(ConnectionOptions.DefaultKey);
-        
+
         Assert.NotNull(connection);
     }
+
+    /// <summary>
+    /// Test GetRequiredService Connection Default FromOptions.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_Connection_Default_FromOptions()
@@ -136,14 +164,17 @@ public class HostBuilderTests
                 options.VirtualHost = "/";
             })
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connection = host.Services.GetRequiredServiceByName<IConnection>(ConnectionOptions.DefaultKey);
-        
+
         Assert.NotNull(connection);
     }
- 
+
+    /// <summary>
+    /// Test GetRequiredService Connection Default FromOptionsBuilder.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_Connection_Default_FromOptionsBuilder()
@@ -155,14 +186,17 @@ public class HostBuilderTests
             })
             .AddRabbitMqConnection(ConnectionOptions.DefaultKey, options => options.BindConfiguration("Path"))
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connection = host.Services.GetRequiredServiceByName<IConnection>(ConnectionOptions.DefaultKey);
-        
+
         Assert.NotNull(connection);
     }
-    
+
+    /// <summary>
+    /// Test GetRequiredService Connection Test FromFactory Test.
+    /// </summary>
     [Fact]
     [IntegrationTest]
     public void Test_GetRequiredService_Connection_Test_FromFactory_Test()
@@ -176,11 +210,11 @@ public class HostBuilderTests
             .AddRabbitMqConnectionFactoryFromOption("test", ConnectionOptions.DefaultKey)
             .AddRabbitMqConnectionFromFactory("test", "test")
             .Build();
-        
+
         Assert.NotNull(host);
 
         var connection = host.Services.GetRequiredServiceByName<IConnection>("test");
-        
+
         Assert.NotNull(connection);
     }
 }
