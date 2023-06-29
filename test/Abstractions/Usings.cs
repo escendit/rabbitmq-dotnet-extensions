e4146ff9f7db2588362613b9c27e4bf897e718ab
Simon Novak <snovak7@gmail.com>
2023-06-29 17:47:01 +0200

    chore: Fixed Usings.cs

--- test/Abstractions/Usings.cs
+++ test/Abstractions/Usings.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Escendit Ltd. All Rights Reserved.
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
 // Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
 
 #pragma warning disable SA1200
